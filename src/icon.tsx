b754072083c5930a1ad42f89c6b8e54749b98883
ambiti0n <fabulousfresh010@gmail.com>
2023-02-08 04:59:36 +0100

    chore: 'mapped icon object'

--- src/icon.tsx
+++ src/icon.tsx
@@ -1,78 +1,32 @@
+type InterfaceIconObj = {
+  id: number;
+  src: string;
+};
+
+const IconObj: InterfaceIconObj[] = [
+  { id: 1, src: "https://img.icons8.com/color/144/000000/react-native.png" },
+  { id: 2, src: "https://img.icons8.com/color/960/000000/typescript.png" },
+  { id: 3, src: "https://img.icons8.com/color/960/000000/nodejs.png" },
+  { id: 4, src: "https://img.icons8.com/color/960/000000/mongodb.png" },
+  { id: 5, src: "https://img.icons8.com/color/480/000000/postgreesql.png" },
+  { id: 6, src: "https://img.icons8.com/color/480/000000/css3.png" },
+  { id: 7, src: "https://img.icons8.com/color/480/000000/sass.png" },
+  { id: 8, src: "https://img.icons8.com/color/960/000000/html-5--v2.png" },
+  { id: 9, src: "https://img.icons8.com/color/480/000000/ethereum.png" },
+  { id: 10, src: "https://img.icons8.com/color/480/000000/firebase.png" },
+  { id: 11, src: "https://img.icons8.com/color/480/000000/graphql.png" },
+  { id: 12, src: "https://img.icons8.com/color/480/000000/git.png" },
+  { id: 13, src: "https://img.icons8.com/color/480/000000/redux.png" },
+  { id: 14, src: "https://img.icons8.com/color/480/000000/docker.png" },
+];
+
 function Icon() {
   return (
     <>
       <div className="item  item-icon">
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/144/000000/react-native.png"
-        />
-        <img className="icon" alt="" src="js.gif" />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/960/000000/typescript.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/960/000000/nodejs.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/960/000000/mongodb.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/postgreesql.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/css3.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/sass.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/960/000000/html-5--v2.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/ethereum.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/firebase.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/graphql.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/git.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/redux.png"
-        />
-        <img
-          className="icon"
-          alt=""
-          src="https://img.icons8.com/color/480/000000/docker.png"
-        />
+        {IconObj.map((icon) => (
+          <img className="icon" alt="" src={icon.src} key={icon.id} />
+        ))}
       </div>
     </>
   );
